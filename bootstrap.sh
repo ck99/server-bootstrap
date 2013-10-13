59697e0a05e31b9e29c33e690dc64697247bed88
Ciaran Kelly <ciaran.kelly@gmail.com>
2013-10-13 14:03:35 +0100

    fix path for bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,6 @@ BOOTSTRAP=~/.server-bootstrap
 
 apt-get update && apt-get install -y git
 
-git clone https://github.com/ck99/server-bootstrap
+git clone https://github.com/ck99/server-bootstrap $BOOTSTRAP
 
 $BOOTSTRAP/auth/add-public-keys.sh
