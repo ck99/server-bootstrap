52b39f7ef5811e18c2053bf4c6fce7d0f52c190b
Ciaran Kelly <ciaran.kelly@gmail.com>
2013-10-13 13:58:24 +0100

    Add bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+BOOTSTRAP=~/.server-bootstrap
+
+apt-get update && apt-get install -y git
+
+git clone https://github.com/ck99/server-bootstrap
+
+$BOOTSTRAP/auth/add-public-keys.sh
