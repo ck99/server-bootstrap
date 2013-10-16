64ba0397a4161df60b7895ed3e64a5e4143d0684
root <static.144-76-118-101.clients.your-server.de>
2013-10-16 22:24:49 +0200

    add default push setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,12 @@ apt-get update && apt-get install -y git
 
 git config --global user.name "Ciaran Kelly"
 git config --global user.email ciaran.kelly@gmail.com
+git config --global push.default simple
 
 git clone git://github.com/ck99/server-bootstrap $BOOTSTRAP
+
+cd $BOOTSTRAP
+git remote set-url origin git@github.com:ck99/server-bootstrap.git
 
 BOOTSTRAP=$BOOTSTRAP $BOOTSTRAP/auth/add-public-keys.sh
 
