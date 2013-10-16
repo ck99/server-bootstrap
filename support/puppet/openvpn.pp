054c35e59cf05ac3bd1985ae7ecce95993bdbdaa
root <static.144-76-118-101.clients.your-server.de>
2013-10-16 21:35:16 +0200

    some new puppet stuff

---
+++ support/puppet/openvpn.pp
@@ -0,0 +1,14 @@
+# add a server instance
+  openvpn::server { 'bitrithm':
+    country      => 'GB',
+    province     => 'SU',
+    city         => 'London',
+    organization => 'bitrithm.co.uk',
+    email        => 'ciaran.kelly@bitrithm.co.uk',
+    server       => '10.37.37.0 255.255.255.0'
+  }
+
+sysctl::directive {
+  "net.ipv4.ip_forward":
+    value => 1;
+}
