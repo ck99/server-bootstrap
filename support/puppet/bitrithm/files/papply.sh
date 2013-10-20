30cf00704ded30f6bf1aa3955a147b41e43c5bd4
Ciaran Kelly <ciaran.kelly@gmail.com>
2013-10-20 02:08:07 +0100

    testing kibana

--- support/puppet/bitrithm/files/papply.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-cd /etc/puppet
-sudo librarian-puppet install
-sudo puppet apply /etc/puppet/manifests/site.pp
--- support/puppet/manifests/testing-nodes.pp
+++ support/puppet/manifests/testing-nodes.pp
@@ -1,3 +1,6 @@
 node 'raring64-vanilla.vagrant' inherits logclient {
-
+  class { 'kibana':
+    webserver   => 'apache',
+    virtualhost => 'logs.bitrithm.co.uk', # Default: kibana.${::domain}
+  }
 }
