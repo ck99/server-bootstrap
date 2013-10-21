3d85f5e0f2af167370f52a7bbff1e731a1168f31
Ciaran Kelly <ciaran.kelly@gmail.com>
2013-10-22 00:59:11 +0100

    change vagrant node definition

--- support/puppet/manifests/testing-nodes.pp
+++ support/puppet/manifests/testing-nodes.pp
@@ -1,6 +1,3 @@
-node 'raring64-vanilla.vagrant' inherits logclient {
-  class { 'kibana':
-    webserver   => 'apache',
-    virtualhost => 'logs.bitrithm.co.uk', # Default: kibana.${::domain}
-  }
+node /^.*\.vagrant/ inherits logclient {
+
 }
