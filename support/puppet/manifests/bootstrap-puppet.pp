6461dbc7b4919dd15078afd9f3e87ad5dc274845
Ciaran Kelly <ciaran.kelly@gmail.com>
2013-10-22 01:52:28 +0100

    more bootstrap

--- support/puppet/manifests/bootstrap-puppet.pp
+++ support/puppet/manifests/bootstrap-puppet.pp
@@ -1,3 +1,3 @@
 $bootstrap_symlinks=hiera('bootstrap_symlinks')
 notice("symlinks: ${bootstrap_symlinks}")
-create_resources(file, $bootstrap_symlinks, {ensure=>'link'})
+create_resources(file, $bootstrap_symlinks, {ensure=>'link', force=>true})
